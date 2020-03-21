b44a9e22cdff81d1f5a0259db734c51fd805c677
kidneyleung <kidneyleung@gmail.com>
2020-03-22 00:59:40 +0800

    test: 测试用例更新

--- test/fixtures/apps/capture-exception/app/controller/home.js
+++ test/fixtures/apps/capture-exception/app/controller/home.js
@@ -4,14 +4,9 @@ const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
   async index() {
-    const { ctx } = this;
-    const message = 'hi, ' + this.app.plugins.loggerSentry.name;
-
     this.app.Sentry.setTag('test', '1');
 
     throw new Error('test');
-
-    ctx.body = message;
   }
 }
 
